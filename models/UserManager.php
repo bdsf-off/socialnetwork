16727f748899a35b4d3e4cc81b7847e0f3068cc2
Bds <essid.badis.pro@gmail.com>
2020-05-27 15:01:19 +0200

    logout feature

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,22 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($login, $password)
+{
+  global $PDO;
+  $prepareRequest = $PDO->prepare("select * from user where nickname=:nickname and password=: id = $id");
+  $prepareRequest->execute(
+    array(
+      "nickname" => $login,
+      "password" => $password
+    )
+  );
+  $users = $preparedRequest->festchall();
+  if (count($users) == 1) {
+    $user = $users[0];
+    return $user['id'];
+  } else {
+    return -1; //on retourne -1 car on est sur qu'il n'y auras pas d'id négatif.
+  }
+}
